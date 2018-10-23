f65058508a62223082ed2e6a543a8b454ecec7fb
Nayana Shetty <nayanashetty@nayanas-mbp.home>
2018-10-23 21:13:36 +0100

    Created a simple flask app that retrives data from a datafile

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+	articles =[
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'This is our first article. Dont have much content otherwise.',
+			'author': 'Nayana Shetty',
+			'create_date': '23-10-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'This is our second article. Dont have much content otherwise.',
+			'author': 'Mad Person',
+			'create_date': '23-10-2018'
+		}
+
+	]
+	return articles
